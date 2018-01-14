585c0567406d995a5dff2d1fed5de1edbe94c103
Adnan Siddiqi <kadnan@gmail.com>
2018-01-14 13:00:42 +0500

    Part 5

---
+++ p5-variables2.py
@@ -0,0 +1,40 @@
+"""
+List, Tuples, Dicts
+"""
+
+# List
+# l = [1, 5, 2, 19, 5, 25]
+# ls = ['A', 'Ad', 'AD']
+# l.append(51)
+# l.append(11)
+# print(l)
+# l.sort()
+# f = l.pop()
+# print(f)
+# print(l)
+
+# # Tuples
+# t = (1, 4, 5)
+# t[1] = 99
+# print(t)
+#
+# import time
+# print(time.localtime())
+
+# Dict
+d = {}
+d = {'name': 'Adnan', 'Education': 'BS', 'Height': 5.8}
+person = []
+person.append({'name': 'Adnan', 'Education': 'BS', 'Height': 5.8})
+person.append({'name': 'Ali', 'Education': 'MA', 'Height': 4.8})
+
+for p in person:
+    print(p['name'], p['Education'])
+
+#
+#
+# t = (1, 2)
+# l = [1, 3, 4, 5, 1, 25, 13]
+# print(time.localtime())
+# l.reverse()
+# print(l)
